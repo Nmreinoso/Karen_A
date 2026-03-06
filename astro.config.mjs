e5bde265ede31647255010fe607a95e3c83f055b
Nmreinoso <nmreinoso@wm.edu>
2026-03-06 07:57:54 -0500

    Add Astro configuration for site and base path

---
+++ astro.config.mjs
@@ -0,0 +1,6 @@
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  site: 'https://Nmreinoso.github.io',
+  base: '/Karen_A', // Important: The name of your GitHub repository
+  output: 'static',
