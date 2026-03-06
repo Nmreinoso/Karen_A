cec27494a6a4f3395dfb4948165d70c40d3054a2
Nmreinoso <nmreinoso@wm.edu>
2026-03-06 09:27:56 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,5 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://Nmreinoso.github.io',
-  base: '/Karen_A',
-  trailingSlash: 'always', // This prevents internal link breakage on GH Pages
-  build: {
-    format: 'directory', // Ensures /resume becomes /resume/index.html
-  }
+  base: '/Karen_A', // This MUST match your repository name exactly
 });
